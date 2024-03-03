30b05f4a8c839e980bf350c6c746cf8b92776aa0
yay <61081228+yanganyi@users.noreply.github.com>
2024-03-04 00:19:48 +0800

    update intro challenges

---
+++ finding_flags.py
@@ -0,0 +1,2 @@
+# simply copy paste the flag into the textbox
+# flag: crypto{y0ur_f1rst_fl4g}
---
+++ great_snakes.py
@@ -0,0 +1,2 @@
+# download and run the python program using $ python <filename>.py
+# flag: crypto{z3n_0f_pyth0n}
